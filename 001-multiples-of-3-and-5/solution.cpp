79493b1c918a5cdae5e2c978092b91c77a8836c9
Kuba Wernerowski <kubawernerowski@gmail.com>
2019-02-25 12:00:55 -0500

    001 done. Added gitignore

---
+++ 001-multiples-of-3-and-5/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  int mult_sum = 0;
+
+  for (int i = 1; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) mult_sum += i;
+  }
+
+  std::cout << mult_sum;
+
+  return 0;
+}
